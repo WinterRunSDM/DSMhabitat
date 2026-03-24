45805b4f179746feec4cd582796254c08fb6cf69
Maddee Rubenson <mrubenson@flowwest.com>
2026-03-24 09:57:12 -0700

    adding spawning and rearing acres for bc-2 action of 3 acres additional

--- data-raw/battle_creek_habitat/cache_battle_creek_habitat.R
+++ data-raw/battle_creek_habitat/cache_battle_creek_habitat.R
@@ -68,6 +68,78 @@ existing_cfs_median_comparison_point <- function (habitat_type, watershed, speci
   }
 }
 
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(ggplot2)
+
+plot_compare_month_year_matrix <- function(x1,
+                                           x2,
+                                           name1 = "Scenario 1",
+                                           name2 = "Scenario 2",
+                                           value_name = "value",
+                                           title = NULL,
+                                           x_lab = "Year",
+                                           y_lab = "Value") {
+  
+  month_levels <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  
+  df1 <- as.data.frame(x1) |>
+    rownames_to_column(var = "month") |>
+    pivot_longer(
+      cols = -month,
+      names_to = "year",
+      values_to = value_name
+    ) |>
+    mutate(
+      year = as.integer(year),
+      month = factor(month, levels = month_levels, ordered = TRUE),
+      dataset = name1
+    )
+  
+  df2 <- as.data.frame(x2) |>
+    rownames_to_column(var = "month") |>
+    pivot_longer(
+      cols = -month,
+      names_to = "year",
+      values_to = value_name
+    ) |>
+    mutate(
+      year = as.integer(year),
+      month = factor(month, levels = month_levels, ordered = TRUE),
+      dataset = name2
+    )
+  
+  df <- bind_rows(df1, df2)
+  
+  p <- ggplot(
+    df,
+    aes(
+      x = year,
+      y = .data[[value_name]],
+      color = dataset,
+      linetype = dataset,
+      group = interaction(month, dataset)
+    )
+  ) +
+    geom_line(linewidth = 0.8) +
+    facet_wrap(~month, ncol = 4) +
+    labs(
+      title = title,
+      x = x_lab,
+      y = y_lab,
+      color = NULL,
+      linetype = NULL
+    ) +
+    theme_minimal()
+  
+  list(
+    data = df,
+    plot = p
+  )
+}
+
 # Battle Creek habitat improvements for Lower Battle Creek
 habitat_projects <- tribble(
   ~"watershed", ~"habitat_type", ~"run", ~"total_acres", ~"gradient_class", ~"percent_suitable",
@@ -93,4 +165,109 @@ thirty_day_mean_exceedence <- existing_cfs_median_comparison_point("floodplain r
                                                                    "action_5")
 # set_habitat <- DSMhabitat::set_floodplain_habitat(watershed, species, thirty_day_mean_exceedence)
 
+# BC-2 add instream acres -------------------------------------------------
+# Here - we are using a similar approach as TMH and baseline hab in R2R where
+# instream and spawning existing WUAs are scaled by the proportion increase 
+# caused by the new habitat acres. The only differences is that instead of 
+# using the median battle creek flow, we use the median WUA flow because 
+# Battle Creek action 5 flows are significatly larger than the WUA flows. 
 
+# 1.3 acres proposed instream habitat - representing Battle Creek Wildlife Area
+# 1.7 acres proposed instream habitat - representing Battle Creek Levee
+
+instream_project_hab_acres <- 1.3 + 1.7
+project_hab_sqmeters <- DSMhabitat::acres_to_square_meters(instream_project_hab_acres)
+
+# MW: the instream and spawning median action 5 flow for battle creek is very high so we are going to scale f
+# rom the median WUA flow instead 
+# instream_flow <- existing_cfs_median_comparison_point("inchannel rearing",
+#                                                       "Battle Creek", "wr",
+#                                                       "action_5") # 672.2546
+
+#instream_habitat_juv <- DSMhabitat::set_instream_habitat("Battle Creek", "wr", life_stage = "juv", instream_flow) # 72526.23
+#instream_habitat_fry <- DSMhabitat::set_instream_habitat("Battle Creek", "wr", life_stage = "fry", instream_flow) # 35411.36
+
+instream_wua_flow <- DSMhabitat::battle_creek_instream |> 
+  select(flow_cfs, WR_fry_wua, WR_juv_wua, WR_spawn_wua) |> 
+  filter(!is.na(flow_cfs)) |> 
+  summarise(median_flow = median(flow_cfs)) |> 
+  pull(median_flow) # 66 cfs
+
+instream_habitat_juv <- DSMhabitat::set_instream_habitat("Battle Creek", "wr", life_stage = "juv", instream_wua_flow) # 104482.3
+instream_habitat_fry <- DSMhabitat::set_instream_habitat("Battle Creek", "wr", life_stage = "fry", instream_wua_flow) # 60410.99
+
+prop_added_juv <- project_hab_sqmeters/instream_habitat_juv
+prop_added_fry <- project_hab_sqmeters/instream_habitat_fry
+
+# update wr_juv
+action_5_bc_2_wr_juv <- DSMhabitat::wr_juv$action_5
+
+add_project_habitat_juv <- DSMhabitat::wr_juv$action_5["Battle Creek" , , ] * prop_added_juv
+updated_habitat_juv <- DSMhabitat::wr_juv$action_5["Battle Creek", , ] + add_project_habitat_juv
+
+action_5_bc_2_wr_juv["Battle Creek", , ] <- updated_habitat_juv
+wr_juv <- modifyList(DSMhabitat::wr_juv, list("action_5_bc_2" = action_5_bc_2_wr_juv))
+usethis::use_data(wr_juv, overwrite = TRUE)
+
+# update wr_fry
+action_5_bc_2_wr_fry <- DSMhabitat::wr_fry$action_5
+
+add_project_habitat_fry <- DSMhabitat::wr_fry$action_5["Battle Creek" , , ] * prop_added_fry
+updated_habitat_fry <- DSMhabitat::wr_fry$action_5["Battle Creek", , ] + add_project_habitat_fry
+
+action_5_bc_2_wr_fry["Battle Creek", , ] <- updated_habitat_fry
+wr_fry <- modifyList(DSMhabitat::wr_fry, list("action_5_bc_2" = action_5_bc_2_wr_fry))
+usethis::use_data(wr_fry, overwrite = TRUE)
+
+# spawning for BC-2
+# spawning_flow <- existing_cfs_median_comparison_point("spawning",
+#                                                       "Battle Creek", "wr",
+#                                                       "action_5") # 302.8045
+# spawning_habitat <- DSMhabitat::set_spawning_habitat("Battle Creek", "wr",  spawning_flow, scenario = NULL) #6080.704
+spawning_habitat <- DSMhabitat::set_spawning_habitat("Battle Creek", "wr",  instream_wua_flow, scenario = NULL) # 13592.19
+
+prop_added_spawn <- project_hab_sqmeters/spawning_habitat
+
+# update wr_spawn
+action_5_bc_2_wr_spawn <- DSMhabitat::wr_spawn$action_5
+
+add_project_habitat_spawn <- DSMhabitat::wr_spawn$action_5["Battle Creek" , , ] * prop_added_spawn
+updated_habitat_spawn <- DSMhabitat::wr_spawn$action_5["Battle Creek", , ] + add_project_habitat_spawn
+
+action_5_bc_2_wr_spawn["Battle Creek", , ] <- updated_habitat_spawn
+wr_spawn <- modifyList(DSMhabitat::wr_spawn, list("action_5_bc_2" = action_5_bc_2_wr_spawn))
+usethis::use_data(wr_spawn, overwrite = TRUE)
+
+# make plots comparing: 
+spawn <- plot_compare_month_year_matrix(
+  x1 = wr_spawn$action_5_bc_2["Battle Creek", , ],
+  x2 = wr_spawn$action_5["Battle Creek", , ],
+  name1 = "Action 5 BC 2",
+  name2 = "Action 5",
+  value_name = "area",
+  title = "Battle Creek - BC-2 spawning"
+)
+
+spawn$plot
+
+juv <- plot_compare_month_year_matrix(
+  x1 = wr_juv$action_5_bc_2["Battle Creek", , ],
+  x2 = wr_juv$action_5["Battle Creek", , ],
+  name1 = "Action 5 BC 2",
+  name2 = "Action 5",
+  value_name = "area",
+  title = "Battle Creek - BC-2 instream juv rearing"
+)
+
+juv$plot
+
+fry <- plot_compare_month_year_matrix(
+  x1 = wr_fry$action_5_bc_2["Battle Creek", , ],
+  x2 = wr_fry$action_5["Battle Creek", , ],
+  name1 = "Action 5 BC 2",
+  name2 = "Action 5",
+  value_name = "area",
+  title = "Battle Creek - BC-2 instream fry rearing"
+)
+
+fry$plot
--- data-raw/update_data.R
+++ data-raw/update_data.R
@@ -10,3 +10,6 @@ source("data-raw/R2R_baseline_habitat_inputs/cache_action5_with_baseline_hab.R")
 source("data-raw/R2R_TMH_habitat_inputs/cache_tmh_data.R")
 source("data-raw/R2R_TMH_habitat_inputs/cache_run_of_river_tmh_data.R")
 source("data-raw/above_shasta_habitat/cache_action_5_tmh_data.R")
+
+# add BC-2 spawning and rearing acres 
+source("data-raw/battle_creek_habitat/cache_battle_creek_habitat.R")
