24efa629d80ab6d253462b53f12de50243abd61a
liz-fw <lstebbins@flowwest.com>
2026-03-24 16:58:03 -0700

    comments out cache scripts that aren't relevant for winter run sdm currently

--- data-raw/update_data.R
+++ data-raw/update_data.R
@@ -3,10 +3,10 @@
 source("data-raw/cached-habitat.R")
 
 # R2R baseline scaling on various hydrologies
-source("data-raw/R2R_baseline_habitat_inputs/cache_baseline_hab.R")
-source("data-raw/R2R_baseline_habitat_inputs/cache_action5_with_baseline_hab.R")
+# source("data-raw/R2R_baseline_habitat_inputs/cache_baseline_hab.R")
+# source("data-raw/R2R_baseline_habitat_inputs/cache_action5_with_baseline_hab.R")
 
 # Max Habitat scaling on various hydrologies
-source("data-raw/R2R_TMH_habitat_inputs/cache_tmh_data.R")
-source("data-raw/R2R_TMH_habitat_inputs/cache_run_of_river_tmh_data.R")
-source("data-raw/above_shasta_habitat/cache_action_5_tmh_data.R")
+# source("data-raw/R2R_TMH_habitat_inputs/cache_tmh_data.R")
+# source("data-raw/R2R_TMH_habitat_inputs/cache_run_of_river_tmh_data.R")
+source("data-raw/above_shasta_habitat/cache_tmh_shasta.R")
