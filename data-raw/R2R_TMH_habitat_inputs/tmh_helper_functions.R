1ddbd9ee44561e017e6704a287145c4548e55b36
liz-fw <lstebbins@flowwest.com>
2026-03-24 15:58:38 -0700

    fixes a few bugs in defaults and tmh processing code

--- data-raw/R2R_TMH_habitat_inputs/tmh_helper_functions.R
+++ data-raw/R2R_TMH_habitat_inputs/tmh_helper_functions.R
@@ -103,7 +103,7 @@ all_existing_and_tmh_data_fun <- function(species, area, extent) {
 }
 
 ## spawning ----------------------------------------------------------------
-spawn_tmh_processing <- function(watersheds, species, calsim_run, area, extent) {
+spawn_tmh_processing <- function(watersheds, species, calsim_run, area = "Standard", extent = 0) {
   
   all_existing_and_tmh_data <- all_existing_and_tmh_data_fun(species, area, extent)
   
@@ -145,7 +145,7 @@ spawn_tmh_processing <- function(watersheds, species, calsim_run, area, extent) 
 }
 
 ## In channel and Fry Rearing ----------------------------------------------------------------
-rearing_tmh_processing <- function(watersheds, species, calsim_run, area, extent) {
+rearing_tmh_processing <- function(watersheds, species, calsim_run, area = "Standard", extent = 0) {
   
   all_existing_and_tmh_data <- all_existing_and_tmh_data_fun(species, area, extent)
   
@@ -194,7 +194,7 @@ rearing_tmh_processing <- function(watersheds, species, calsim_run, area, extent
 }
 
 ## floodplain  ----------------------------------------------------------------
-floodplain_tmh_processing <- function(watersheds, species, calsim_run, area, extent) {
+floodplain_tmh_processing <- function(watersheds, species, calsim_run, area = "Standard", extent = 0) {
   
   all_existing_and_tmh_data <- all_existing_and_tmh_data_fun(species, area, extent)
   
@@ -355,7 +355,7 @@ existing_flow_cfs <- function(habitat_type, watershed_input, bypass = FALSE, spe
     }
   } else if(habitat_type == "flood") {
     # TODO: this is a hacky fix, unsure about it... MW 3/4/26
-    if(watershed_input %in% c("Lower-mid Sacramento River1", "Lower-mid Sacramento River2")) {watershed_input <- "Lower-mid Sacramento River"}
+    if(str_detect(calsim_run, "action_5") & watershed_input %in% c("Lower-mid Sacramento River1", "Lower-mid Sacramento River2")) {watershed_input <- "Lower-mid Sacramento River"}
     flood = flow_df |> 
       filter(date >= as_date("1980-01-01")) |> 
       select(watershed_input, date) |> 
