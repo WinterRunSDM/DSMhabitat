2bb83b063db395120e5ea39a0544407cc86a5060
Maddee Rubenson <mrubenson@flowwest.com>
2026-03-24 11:59:22 -0700

    updating scripts with scenario default to null

--- data-raw/cached-habitat.R
+++ data-raw/cached-habitat.R
@@ -40,7 +40,7 @@ get_flow <- function(watershed, calsim_version, years = c(1980, 1999)) {
 }
 
 # get rearing habitat for all watersheds 
-get_rear_hab_all <- function(watersheds, species, life_stage, calsim_version, years = 1980:1999, scenario) {
+get_rear_hab_all <- function(watersheds, species, life_stage, calsim_version, years = 1980:1999, scenario = NULL) {
   total_obs <- 12 * length(years)
   most <- map_df(watersheds, function(watershed) {
     flows <- get_flow(watershed, calsim_version, range(years))
@@ -137,7 +137,7 @@ get_rear_hab_all <- function(watersheds, species, life_stage, calsim_version, ye
 }
 
 # get spawning habitat for all watersheds
-get_spawn_hab_all <- function(watersheds, species, calsim_version, years = 1979:2000, scenario) {
+get_spawn_hab_all <- function(watersheds, species, calsim_version, years = 1979:2000, scenario = NULL) {
   total_obs <- 12 * length(years)
   most <- map_df(watersheds, function(watershed) {
     flows <- get_flow(watershed, calsim_version, years=range(years))
@@ -293,22 +293,22 @@ spawning_watersheds <- DSMhabitat::watershed_species_present %>%
 
 # fall run spawning habitat --
 # fr spawn 2008 2009 
-fr_spawn_2008_2009 <- get_spawn_hab_all(spawning_watersheds, 'fr', "biop_2008_2009")
+fr_spawn_2008_2009 <- get_spawn_hab_all(spawning_watersheds, 'fr', "biop_2008_2009", scenario = NULL)
 dimnames(fr_spawn_2008_2009) <- list(watersheds, month.abb, 1979:2000)
 fr_spawn_2008_2009[which(is.na(fr_spawn_2008_2009))] <- 0
 
 # fr spawn 2018 2019 
-fr_spawn_2018_2019 <- get_spawn_hab_all(spawning_watersheds, 'fr', "biop_itp_2018_2019")
+fr_spawn_2018_2019 <- get_spawn_hab_all(spawning_watersheds, 'fr', "biop_itp_2018_2019", scenario = NULL)
 dimnames(fr_spawn_2018_2019) <- list(watersheds, month.abb, 1979:2000)
 fr_spawn_2018_2019[which(is.na(fr_spawn_2018_2019))] <- 0
 
 # fr spawn run of river
-fr_spawn_run_of_river <- get_spawn_hab_all(spawning_watersheds, 'fr', "run_of_river")
+fr_spawn_run_of_river <- get_spawn_hab_all(spawning_watersheds, 'fr', "run_of_river", scenario = NULL)
 dimnames(fr_spawn_run_of_river) <- list(watersheds, month.abb, 1979:2000)
 fr_spawn_run_of_river[which(is.na(fr_spawn_run_of_river))] <- 0
 
 # fr spawn action 5
-fr_spawn_action_5 <- get_spawn_hab_all(spawning_watersheds, 'fr', "action_5")
+fr_spawn_action_5 <- get_spawn_hab_all(spawning_watersheds, 'fr', "action_5", scenario = NULL)
 dimnames(fr_spawn_action_5) <- list(watersheds, month.abb, 1979:2000)
 fr_spawn_action_5[which(is.na(fr_spawn_action_5))] <- 0
 
